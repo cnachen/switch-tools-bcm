426bd003a89fab768f2f7c05fd81198e814e9acc
ningan.chen <ningan.chen@momenta.ai>
2024-06-26 13:22:15 +0800

    [feat]: Update switch_tool_bcm example program

--- switch_tool_bcm.c
+++ switch_tool_bcm.c
@@ -1,16 +1,40 @@
 #include <stdio.h>
+#include <string.h>
 
 #include "switch_tool_bcm.h"
 
-int main()
+#define VERSION "1.0.0"
+#define BUFFER_SIZE 128
+
+int main(int argc, char *argv[])
 {
-    char buffer[114];
-    get_switch_version(buffer, 114);
-    printf("%s\n", buffer);
+    if (argc == 1)
+        goto help;
 
-    flash_switch("/tmp/fwnt.img");
-    printf("flash done\n");
+    for (int i = 1; i < argc; i++) {
+        if (!strcmp(argv[i], "-h")) {
+            goto help;
+        } else if (!strcmp(argv[i], "-v")) {
+            char buffer[BUFFER_SIZE];
+            if (get_switch_version(buffer, BUFFER_SIZE)) {
+                printf("get_switch_version failed...\n");
+                return 1;
+            }
+            printf("%s\n", buffer);
+        } else if (!strcmp(argv[i], "-f")) {
+            if (i + 1 >= argc || flash_switch(argv[++i])) {
+                printf("flash_switch failed...\n");
+                return 1;
+            }
+        }
+    }
 
-    get_switch_version(buffer, 114);
-    printf("%s\n", buffer);
+    return 0;
+help:
+    printf("stb %s\n", VERSION);
+    printf("Usage: %s <OPTION>\n", argv[0]);
+    printf("Options:\n");
+    printf("\t-h\t\tPrint this help\n");
+    printf("\t-v\t\tGet switch version\n");
+    printf("\t-f <FILE>\tFlash switch firmware\n");
 }
